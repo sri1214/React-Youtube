4bca0ca7569005fe74780bab1b67120f55ce9d23
Srikanth Vinukonda <Srikanthchandu.Vinukonda@aa.com>
2017-09-01 16:56:35 -0500

    initial setup

---
+++ app/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import css from './index.css'
+
+function Main(){
+  return <div>Hello World</div>
+}
+
+ReactDOM.render(<Main />, document.getElementById('app'));
